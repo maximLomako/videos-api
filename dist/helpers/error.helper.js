811dba464f9ba91d0864f208b9257dd26b924cac
maximLomako <maxim.lomako@icloud.com>
2022-05-10 21:42:30 +0400

    fixed: removed dist

--- dist/helpers/error.helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getErrorAnswer = void 0;
-const getErrorAnswer = (message, field) => {
-    return { message, field };
-};
-exports.getErrorAnswer = getErrorAnswer;
-//# sourceMappingURL=error.helper.js.map
